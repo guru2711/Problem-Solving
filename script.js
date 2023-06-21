0f7122f2665bd5201df727de9a3467abf8aea9b1
Guru <59472229+guru2711@users.noreply.github.com>
2023-06-21 21:31:46 +0530

    Update frequencyCounter.js

--- script.js
+++ script.js
@@ -1,73 +1,32 @@
-/**Books obj are be stored i arr */
-let myLibrary = [];
+function sameFrequency(valA,valB){
+  // good luck. Add any arguments you deem necessary.
+  let val1 = valA.toString();
+  let val2 = valB.toString();
+ 
+  if(val1.length && val2.length === 0 ) return false;
+  let obj1 = {};
+  let obj2 = {};
+  if(val1.length === val2.length){
+      for(let i = 0 ; i < val1.length; i++){
+          obj1[val1[i]] = obj1[val1[i]] + 1 || 1;
+          obj2[val2[i]] = obj2[val2[i]] + 1 || 1;
+      }
 
-const showBtn = document.querySelector(".btn_show");
-const addBook = document.querySelector(".btn_addBook");
-const details = document.querySelector(".details");
-const showBook = document.querySelector(".showBook");
-const form = document.querySelector(".form");
-const tbody = document.querySelector("tbody");
+   for(let i in obj1){
+   
+     if(obj1[i] !== obj2[i]) {
 
-function Book() {
-  tbody.innerHTML = "";
-  let i = 1,
-    a;
-  for (let book of myLibrary) {
-    // cconcept table
-    a = i;
-    const tr = document.createElement("tr");
-    tr.className = "tr";
-    tr.innerHTML = `
-    <td>${i++}</td>
-    <td>${book.author}</td>
-    <td>${book.title}</td>
-    <td><input type="checkbox" name="status"> <button  class="btnDel">Del</button></td>
-    `;
-    tbody.append(tr);
+       return false
+     }
+   }
+    
+  
+return true;
   }
-  const tn = document.querySelectorAll(".btnDel");
-  let getCurr_El = [...tn];
-
-  console.log(getCurr_El);
-  getCurr_El.map((e, i) =>
-    e.addEventListener("click", () => {
-      console.log(e, i);
-      myLibrary.splice(i, 1);
-      Book();
-      // e.remove();
-      // e.target.parentElement.parentElement.remove();
-    })
-  );
+  return false;
 }
-// showBtn.addEventListener("click", Book);
 
-// add Book
-addBook.addEventListener("click", addBookToLibrary);
-
-const addTitle = document.querySelector("input[name=title]");
-const addAuthor = document.querySelector("input[name=author]");
-const addPages = document.querySelector("input[name=pages]");
-const addStatus = document.querySelector("#status");
-
-function addBookToLibrary(e) {
-  e.preventDefault();
-  // user inputs (books) to library
-
-  const title = addTitle.value;
-  const author = addAuthor.value;
-  const status = addStatus;
-
-  const book = { author, title, status };
-  // if (title.length > 0) {
-  myLibrary.push(book);
-  Book();
-  clear();
-  // }
-
-  function clear() {
-    addTitle.value = "";
-    addAuthor.value = "";
-    addPages.value = "";
-  }
-  console.table(myLibrary);
-}
+// sameFrequency(182,281) // true
+// sameFrequency(34,14) // false
+// sameFrequency(3589578, 5879385) // true
+sameFrequency(22,222) // false
