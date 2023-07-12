39bb8d4b7f66f63883770e2b7a83dafaf7d54558
Guru <59472229+guru2711@users.noreply.github.com>
2023-07-12 20:55:35 +0530

    Update patterns.js

--- patterns.js
+++ patterns.js
@@ -32,22 +32,4 @@ function areThereDuplicates() {
   }
   return false;
 }
-areThereDuplicates Solution (Multiple Pointers)
-function areThereDuplicates(...args) {
-  // Two pointers
-  args.sort((a,b) => a > b);
-  let start = 0;
-  let next = 1;
-  while(next < args.length){
-    if(args[start] === args[next]){
-        return true
-    }
-    start++
-    next++
-  }
-  return false
-}
-areThereDuplicates One Liner Solution
-function areThereDuplicates() {
-  return new Set(arguments).size !== arguments.length;
-}
+
